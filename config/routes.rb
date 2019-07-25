999fdea5d6d46e7127778a1eef7fe85ce5c25d41
ty <uw.ty0cu21@gmail.com>
2019-07-25 10:48:43 +0000

    log in 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
